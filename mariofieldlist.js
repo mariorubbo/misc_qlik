1e770f9dd7e8244b6adeb8e7f5fab7bbf6b149dc
mariorubbo <34013204+mariorubbo@users.noreply.github.com>
2018-07-12 21:14:01 -0400

    Add files via upload

---
+++ mariofieldlist.js
@@ -0,0 +1,37 @@
+const createSession = require('./session');
+
+const script = `
+TempTable:
+Load
+RecNo() as mario1,
+Rand() as mario2,
+Rand() as mario3
+AutoGenerate 100
+`;
+
+const properties = {
+  qInfo: {
+    qType: 'my-field-list',
+  },
+  qFieldListDef: {},
+};
+
+const session = createSession();
+
+// Open the session and create a session document:
+session
+  .open()
+  .then(global => global.createSessionApp())
+  .then(doc => doc
+    .setScript(script)
+    .then(() => doc.doReload())
+    .then(() => doc
+      // Create a field list using qFieldListDef and list all fields available in the document.
+      .createObject(properties)
+      .then(object => object.getLayout())
+      .then(layout => console.log('Field list:', JSON.stringify(layout, null, '  ')))
+      .then(() => session.close())))
+  .catch((error) => {
+    console.log('Session: Failed to open socket:', error);
+    process.exit(1);
+  });
---
+++ mariothird.js
@@ -0,0 +1,87 @@
+
+const createSession = require('./session');
+
+
+
+const qlikScript = `
+TempTable:
+Load
+RecNo()+1 as ID2,
+Rand() as Value
+AutoGenerate 100
+`;
+
+const properties = {
+  qInfo: {
+    qType: 'my-straight-hypercube',
+  },
+  qHyperCubeDef: {
+    qDimensions: [
+      {
+        qDef: { qFieldDefs: ['ID'] },
+      },
+      {
+        qDef: { qFieldDefs: ['ID2'] },
+      },
+    ],
+    qMeasures: [
+      {
+        qDef: { qDef: '=Sum(Value)' },
+      },
+    ],
+    qMode: 'EQ_DATA_MODE_PIVOT_STACK',
+    qAlwaysFullyExpanded: true,
+  },
+};
+
+
+const session = createSession();
+
+
+
+// Open the session and create a session document:
+session.open()
+.then(global => global.createSessionApp())
+
+.then(doc => doc.setScript(qlikScript)
+    .then(() => doc.doReload())
+ 
+    // Create a generic object with a hypercube definition containing one dimension and one measure
+    .then(() => doc.createObject(properties))
+ 
+    // Get hypercube layout
+    .then(object => object.getHyperCubeStackData('/qHyperCubeDef', [
+      {
+        qTop: 0,
+        qLeft: 0,
+        qHeight: 5,
+        qWidth: 2,
+      },
+    ])
+
+    .then(data => console.log('Hypercube data pages:', JSON.stringify(data, null, '  ')))
+ 
+    // Select second value in the first column of the data matrix
+    .then(() => object.selectPivotCells('/qHyperCubeDef', [{
+      qType: 'D',
+      qRow: 1,
+      qCol: 0,
+    }], false))
+    
+    // Get pivot data
+    .then(() => object.getHyperCubeStackData('/qHyperCubeDef', [
+      {
+        qTop: 0,
+        qLeft: 0,
+        qHeight: 5,
+        qWidth: 2,
+      },
+    ]).then(data => console.log('Hypercube data pages after selection:', JSON.stringify(data, null, '  '))))))
+
+ 
+    // Close the session
+    .then(() => session.close())
+    .catch((error) => {
+    console.log('Session: Failed to open socket:', error);
+    process.exit(1);
+});
